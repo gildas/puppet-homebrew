889b6486bcece48893ab0254210e708306fe1aad
John Eckhart <jeckhart@yodle.com>
2014-08-17 20:41:27 -0400

    xcodeversion fact always tries to install xcode

--- lib/facter/xcodeversion.rb
+++ lib/facter/xcodeversion.rb
@@ -1,10 +1,23 @@
 Facter.add(:xcodeversion) do
   confine :operatingsystem => :darwin
   setcode do
-    return "" if ! File.exists?('/usr/bin/xcodebuild')
-    results = %x{ /usr/bin/xcodebuild -version 2>&1 }
-    return "" if ! results =~ /^Xcode\s((?:\d+\.)?(?:\d+\.)?\d+)/
-    $1
+    if File.exists?('/usr/bin/xcodebuild')
+      results = %x{ /usr/bin/xcodebuild -version 2>&1 }
+      if ! results =~ /^Xcode\s((?:\d+\.)?(?:\d+\.)?\d+)/
+        $1
+      end
+    end
   end
-end
+  
+  # At least in Maverics, if you even run xcorebuild it will try to install the tools
+  confine :macosx_productversion_major => "10.9"
+  setcode do
+    if File.exists?('/Applications/Xcode.app') or File.exists?('/Library/Developer/CommandLineTools/')
+      results = %x{ /usr/bin/xcodebuild -version 2>&1 }
+      if results =~ /^Xcode\s((?:\d+\.)?(?:\d+\.)?\d+)/
+        $1
+      end
+    end
+  end
 
+end
